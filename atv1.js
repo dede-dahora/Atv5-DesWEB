4113971ccc1f4e293c268c8ecb78dc622fd9d5d0
dede-dahora <andresoares@colegiopaulodetarso.com.br>
2025-06-02 15:40:43 +0000

    commit hehe

--- atv1.js
+++ atv1.js
@@ -1,6 +1,5 @@
-const array1 = [10, 20, 30, 40];
-
-console.log("Primeiro elemento:", array1[0]); // 10
-console.log("Último elemento:", array1[array1.length - 1]); // 40
-console.log("Tamanho do array:", array1.length); // 4
-console.log("Elemento inexistente:", array1[10]); // undefined
+const array1 = ["atemoia", "pitaya", "mirtilo", "goiaba"];
+console.log(array1);
+console.log(array1[0]);
+console.log(array1[3]);
+console.log(array1.length);
